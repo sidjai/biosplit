efaebe15b01c0ac47802791c2404c3a6896cbbea
Siddarta Jairam <sidjsb@gmail.com>
2015-05-20 16:15:20 -0500

    Script working: ARL2GRD
    -Got the function runScript working as a new file with a closure since everyone will have different location for programs.
    
    -Made ARL2Grd work with new file systems and with commandline arguments

---
+++ R/runScript.R
@@ -0,0 +1,60 @@
+#' A functionfactory for running scripts in this package
+#' 
+#' An OS agnostic way of calling a script from the command line given the
+#' location of the program file. The scripts are either in the scripts folder or
+#'  the individual languges. The py extension refers to the Jython scripts that 
+#'  are used with MeteoInfo which needs to eb installed beforehand. The BAS 
+#'  extension is used with Golden Software's Surfer's automation tool, scripter.
+#'  
+#'@param driver The file location of the root folder of the desired function.
+#'@param ext The extension that the function is to be used with (py, BAS or R)
+#'@export
+makeRunFun <- function(driver, ext){
+	langFlag <- switch(ext, R=1,BAS=2,py=3,-9999)
+	if (langFlag == -9999){
+		stop(paste("pkg doesn't have any", ext, "files"))
+	}
+	
+	scrDir <- system.file(package = "biosplit")
+	
+	scrDir <- paste(scrDir, 
+								 switch(ext, R="scripts", BAS="surfer", py="jython")
+								 ,sep='/')
+	
+	driver <- if(langFlag == 1){
+		#R scripts
+		 quoteSt(paste(driver,"bin","Rscript.exe",sep="/"))
+	} else if(langFlag ==2){
+		paste(quoteSt(paste(driver,"Scripter","Scripter.exe",sep="/")), "-x")
+	} else {
+		quoteSt(paste(meteoLoc,"meteoinfo.bat",sep="/"))
+	}
+	
+	return(function(scrName, args="",
+									verbose = FALSE){
+		
+		if (!grepl(paste0('[.]', ext), scrName)){
+			stop(paste("Code:", scrName, "| is not of a supported language"))
+		}
+		
+		if (!file.exists(paste(scrDir, scrName, sep="/"))){
+			stop(paste("Code:", scrName, "| Does not exist"))
+		}
+		
+		command <- paste("CD", scrDir)
+		
+		command[2] <- paste(driver, scrName, paste(quoteSt(args), collapse = " "))
+		
+		input <- paste(command,collapse=" && ")
+		
+		cat("\n","#######", paste("calling", scrName),"\n")
+		if(verbose) cat(input)
+		
+		jk <- shell(input,mustWork=TRUE)
+		
+		
+	})
+}
+quoteSt <- function(instr){
+	paste0("\"", instr, "\"")
+}
--- inst/jython/ARL2GRD.py
+++ inst/jython/ARL2GRD.py
@@ -10,45 +10,21 @@ import sys
 import os
 import re
 
-configLoc = os.path.dirname(os.path.realpath('__file__')).replace("Python_code","cfg.txt")
-configTxt = open(configLoc)
-raw = configTxt.readlines()[1:]
-cfg = {"x":"hello"}
-for line in raw:
-    nice = line.strip('"').strip('"\n').split(' ="')
-
-    key = nice[1].strip(' "')
-    if re.search('c\(\\\\',key) is not None:
-        key = re.findall('[^"]+',key)[1::2]
-    
-    cfg.update({nice[0]:key})
+dirIn = sys.argv[1]
+dirOut = sys.argv[2]
+wantVars = sys.argv[3:]
 
 Met = MeteoDataInfo()
 
-Var = {"T":"T02M", 
-	"W":"V10M", 
-	"S":"SOLT"}
-Lookup = {"T":"AirTempFold", 
-	"W":"WindFold", 
-	"S":"SoilTempFold"}
-resName = {"T":"T2M", 
-	"W":"VdirWind10M", 
-	"S":"Tsoil"}
+yearstr = re.findall('\d{4}',dirIn)[0]
+metType = os.path.split(dirIn)[1]
 
+print metType
 
-months = 12
-shYear = str(int(cfg["year"])-2000)
+months = 12 
+shYear = str(int(yearstr)-2000)
 
-
-#from:lat 25, lon -110
-#to:50,-80
-
-xmin = float(cfg["xmin"])
-xmax = float(cfg["xmax"])
-ymin = float(cfg["ymin"])
-ymax = float(cfg["ymax"])
-spc = float(cfg["spc"])
-
+dirOut = (dirOut + '/' + yearstr + '/')
 
 def zstr(num):
 	#return the string of the number including a zero at beg
@@ -67,7 +43,7 @@ for mInd in [d+1 for d in range(months)]:
 	ds=[None]*250
 	# iterate through the days and get the days, hours and the time index
 	# Grabs the index for midnight and 3PM as the min and max temps for the day
-	for di in range(1,monthrange(int(cfg["year"]),mInd)[1]+1):
+	for di in range(1,monthrange(int(yearstr),mInd)[1]+1):
 		hs[k]=0
 		ds[k]=di
 		if k!=1:
@@ -81,32 +57,25 @@ for mInd in [d+1 for d in range(months)]:
 		inds[k]=inds[k-1]+5
 		k+=1
 		
-	inFile = (cfg["MetARLFold"] + "/" + cfg["metDataType"] + "." +
+	inFile = (dirIn + "/" + metType + "." +
 		(month_abbr[mInd]).lower() + shYear)
+		
 	
 	print(inFile)
 	
 	if os.path.isfile(inFile):
-		#Load the file and project it to NASS
+		#Load the file
 		Met.openARLData(inFile)
-
-		orgProj = Met.getProjectionInfo()
-		print(orgProj.toProj4String())
       
 		#Go through all the collected time indices and save the data in Parsed as a grid file.
 		for tInd in range(1,k):
 			Met.setTimeIndex(inds[tInd])
-			for vInd in Var:
-				
-				fileName = (cfg["metDataType"] + "_" + resName[vInd] + "_" +
+			for var in wantVars:
+				pathOut = (dirOut + var + "/Parsed/" + metType + "_" + var +"_" +
 					zstr(mInd)+ "_" + zstr(ds[tInd]) + "_" + shYear
-					+"_at_"+zstr(hs[tInd]))
-				outFile = (cfg[Lookup[vInd]][0] + "/" + fileName )
-				
-				outFileDat = outFile + ".dat"
-				outFileGrd = outFile + ".grd"
+					+"_at_" + zstr(hs[tInd]) + ".grd")
 				
 				#save 
-				datu = Met.getGridData(Var[vInd])
-				datu.saveAsSurferASCIIFile(outFileGrd)
+				datu = Met.getGridData(var)
+				datu.saveAsSurferASCIIFile(pathOut)
 				
