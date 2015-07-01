514ea9da086746074508546258ec6021fc065895
Siddarta Jairam <sidjsb@gmail.com>
2015-07-01 16:17:26 -0500

    Add trap scrubbing
    -notes may or may not be working
    -need to add more notes

--- R/scrubValidation.R
+++ R/scrubValidation.R
@@ -129,3 +129,109 @@ convDaysAfter1900 <- function(days){
 	jd <- (days - dayOff)
 	return(jd)
 }
+
+scrubTrap <- function(pathXlsx, year, pestType = "faw", minTotCatch = 10){
+	trapColType <- rep("numeric",10)
+	trapColType[4:5] <- "text"
+	trapColType[6] <- "date"
+	trapColType[7] <- "text"
+	trapColType[10] <- "text"
+	
+	sheet <- readxl::read_excel(pathXlsx, sheet = 1, col_types = trapColType)
+	
+	#basic data cutoff
+	
+	inYr <- as.integer(strftime(sheet[,6], "%Y"))
+	
+	badYearSet <- (inYr != year)
+	badPestSet <- !grepl(pestType, sheet[,7])
+	noCountySet <- is.na(sheet[,4])
+	
+	usefulEle <- sheet[!(badYearSet | badPestSet | noCountySet), -10]
+	usefulEle[,6] <- as.integer(strftime(usefulEle[,6], "%j"))
+	
+	usefulEle <- usefulEle[order(usefulEle[,1], usefulEle[,6]),]
+	
+	current <- rev(rev(usefulEle$farmid)[-1])
+	nextEle <- usefulEle$farmid[-1]
+	
+	farmEndInd <- which(current != nextEle)
+	farmStaInd <- c(as.integer(1), rev(rev(farmInd)[-1]))
+	
+	statDat <- t(mapply(function(beg, last){
+		catches <- usefulEle[beg:last, 8]
+		totCatch <- sum(catches)
+		firstOccInd <- which(catches>0)[1]
+		firstOcc <- usefulEle[(beg:last)[firstOccInd], 6]
+		return(c(totCatch, firstOcc))
+	}, farmStaInd, farmEndInd))
+	
+	badFarm <- (statDat[,1] < minTotCatch)
+	
+	statDat <- statDat[!badFarm,]
+	farmEndInd <- farmEndInd[!badFarm]
+	farmStaInd <- farmStaInd[!badFarm]
+	
+	
+	statID <- paste(usefulEle$countyname[farmStaInd], usefulEle$stateabbreviation[farmStaInd], sep = ", ")
+	
+	
+	records <- list()
+	for (rind in 1:length(farmEndInd)){
+		rseq <- farmStaInd[rind]:farmEndInd[rind]
+		
+		temp <- getTrapTimeSeries(usefulEle[rseq, 8],
+															usefulEle[rseq, 6], 
+															usefulEle[rseq, 9])
+		
+		
+		newRec <- list(ID = statID[rind],
+									 latlon = c(usefulEle[farmStaInd[rind],2],
+									 					 -usefulEle[farmStaInd[rind],3]),
+									 totCatch = statDat[rind, 1],
+									 firstOcc = statDat[rind, 2],
+									 capTSer = temp[[1]],
+									 notes = temp[[2]]
+		)
+									 
+									 
+		sameCountySet <- grepl(statID[rind], vapply(records,function(x) {x$ID},"e"))
+		if(any(sameCountySet)){
+			oldRecInd <- which(sameCountySet)[1]
+			if(statDat[rind,1] > records[[oldRecInd]]$totCatch){
+				records[[oldRecInd]] <- newRec
+			}
+			
+		} else {
+			records <- lappend(records, newRec)
+		}
+	}
+	
+	
+	return(invisible(records))
+}
+
+getTrapTimeSeries <- function(catches, jds, periods){
+	days <- seq(8,365,7)
+	notes <- "None"
+	
+	#get rid of articifically low catch periods because of pestWatch limit of 14
+	
+	#normalize to weekly captures
+	catches <- round(catches * 7 / periods, 1)
+	
+	numWeeks <- ceiling(periods / 7)
+	moreWeekSet <- (numWeeks > 1)
+	catches <- c(catches, rep(catches[moreWeekSet], numWeeks[moreWeekSet]))
+	jds <- c(jds, rep(jds[moreWeekSet], numWeeks[moreWeekSet]))
+	
+	catInd <- vapply(jds, function(x){
+		which(days > x)[1]
+	}, 1)
+	
+	tSer <- rep(0, 52)
+	tSer[catInd] <- catches
+	
+	return(list(tSer, notes))
+	
+}
