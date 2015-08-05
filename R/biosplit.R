421767514122d3d121ae44e75773ab6d3d11f986
Siddarta Jairam <sidjsb@gmail.com>
2015-08-05 14:09:16 -0500

    add package documentation

---
+++ R/biosplit.R
@@ -0,0 +1,11 @@
+#' biosplit: A model for national insect migration
+#' 
+#' biosplit package provides functions for modeling the biological growth of 
+#' insects and their food source as well as nightly flight using dispersion 
+#' modeling from the HYSPLIT model from NOAA. Included are all the tools needed
+#' to collect the data required, run the model and analye the results. For more 
+#' information on how all the functions work you can go to vignette/workflow.rmd
+#' 
+#' @docType package
+#' @name biosplit
+NULL
