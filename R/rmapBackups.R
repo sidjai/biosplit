0547677cb54192606ded989aa6771e5fea03b46c
Siddarta Jairam <sidjsb@gmail.com>
2015-10-06 01:10:57 -0700

    add base map
    - made the maps work as a package
    - decided to just use "maps" package for the base map which needs to be imported because R and some data shenegians having to do with the map package.
    -  space -> tab

--- R/rmapBackups.R
+++ R/rmapBackups.R
@@ -1,93 +1,103 @@
-intwBaseMap <- function(bbox = c()){
-  plot <- usmap(bbox)
-  return(plot)
+#' Make a diagonstic map for a layer
+#'
+#' A one layer map with the designated layer.
+#' @param arr Anything that can be loaded in by 'raster' package
+#' @param type The type of map that you want printe
+#' @param pathSave The path that you want the saved jpeg to be in. Default is that the function does not save but just plot
+#'
+#' @return A plot with 2 layers, the base map and the array
+#' @export
+makeDiagnosticMap <- function(
+	arr,
+	type = c("contour", "post")[1],
+	pathSave = ""
+	){
 
+	ras <- parseLayerInput(arr)
+
+	pl <- intwBaseMap(bbox = raster::extent(ras)[])
+	baseOpt <- intBaseOpt()
+
+
+	pl <- addLayer(type, ras, opt)
+
+	if(nzchar(save)){
+		saveMapJpg(pl, save)
+	}
+	return(pl)
+}
+
+#' @import maps
+intwBaseMap <- function(bbox){
+	map("state", xlim = bbox[1:2], ylim = bbox[3:4])
 }
 
 intBaseOpt <- function(){
-  opt <- list()
+	opt <- list()
 }
 
 addLayer <- function(type, layer, opt){
 
-  if(type = "contour"){
-    raster::contour(
-      layer,
-      levels = opt$levels,
-      add = TRUE)
-  } else {
-    posts <- postMaperize(layer, classes = opt$classes)
-    points(
-      posts$pts,
-      bg = points$color,
-      pch = points$shape,
-      add = TRUE)
-  }
+	if(type == "contour"){
+		raster::contour(
+			layer,
+			levels = opt$levels,
+			add = TRUE)
+	} else {
+		posts <- postMaperize(layer, classes = opt$classes)
+		points(
+			posts$pts,
+			bg = points$color,
+			pch = points$shape,
+			add = TRUE)
+	}
 
 }
 
 postMaperize <- function(layer, classes = matrix(NA, nrow = 5, ncol = 3)){
-  ptsSet <- (layer > thres)
-  ptsMat <- which(ptsSet, arr.ind = TRUE)
+	ptsSet <- (layer > thres)
+	ptsMat <- which(ptsSet, arr.ind = TRUE)
 
-  #Translate the x y coordinates
+	#Translate the x y coordinates
 
-  colnames(classes) <- c("Begin Interval", "Symbol", "Color")
-  badSet <- vapply(colnames(classes), function(x){
-    is.na(classes[, x])
-  }, TRUE)
+	colnames(classes) <- c("Begin Interval", "Symbol", "Color")
+	badSet <- vapply(colnames(classes), function(x){
+		is.na(classes[, x])
+	}, TRUE)
 
-  bot <- min(layers)
-  top <- max(layers)
-  classes[badSet[, "Begin Interval"], "Begin Interval"] <- {
-    seq(bot, top, by = top/5)[badSet[, "Begin Interval"]}
+	bot <- min(layers)
+	top <- max(layers)
+	classes[badSet[, "Begin Interval"], "Begin Interval"] <- {
+		seq(bot, top, by = top/5)[badSet[, "Begin Interval"]]}
 
-  classes[badSet[, "Symbol"], "Symbol"] <- c(1, 0, 2, 5, 11)[badSet[, "Symbol"]]
+	classes[badSet[, "Symbol"], "Symbol"] <- c(1, 0, 2, 5, 11)[badSet[, "Symbol"]]
 
-  classes[badSet[, "Color"], "Color"] <- rep("black", sum(badSet[, "Symbol"]))
+	classes[badSet[, "Color"], "Color"] <- rep("black", sum(badSet[, "Symbol"]))
 
 
-  colMat <- getFromClass(ptsMat[, 3], classes[, "Color"])
-  shapeMat <- getFromClass(ptsMat[, 3], classes[, "Shape"])
+	colMat <- getFromClass(ptsMat[, 3], classes[, "Color"])
+	shapeMat <- getFromClass(ptsMat[, 3], classes[, "Shape"])
 
-  return(list(
-    pts = ptsMat,
-    color = colMat,
-    shape = shapeMat
-  ))
+	return(list(
+		pts = ptsMat,
+		color = colMat,
+		shape = shapeMat
+	))
 }
 
 getFromClass <- function(pts, cvec){
-  return(cvec[findInterval(pts, cvec)])
+	return(cvec[findInterval(pts, cvec)])
 }
 
 saveMapJpg <- function(map, path){
-  JPEG(map, path)
+	JPEG(map, path)
 }
 
 parseLayerInput <- function(mapin){
-  fir <- raster(mapin)
-  if(~is.raster(fir)){
-    #do something damn it
-  }
-}
+	fir <- tryCatch(
+		raster::raster(mapin),
+		finally = function(cond) {error("Input cannot be read by raster with this error",
+			cond, "/n") }
 
-makeDiagnosticMap <- function(
-  arr,
-  type = c("contour", "post")[1],
-  pathSave = ""
-  ){
-
-  ras <- parseLayerInput(arr)
-
-  pl <- intwBaseMap()
-  baseOpt <- intBaseOpt()
-
-
-  pl <- addLayer(type, ras, opt)
-
-  if(nzchar(save)){
-    saveMapJpg(pl, save)
-  }
-  return(pl)
+	)
 }
