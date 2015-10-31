3a375c01b6a24153bf77a2d825a73994ee652c42
Siddarta Jairam <sidjsb@gmail.com>
2015-10-31 15:08:12 -0700

    1 file to 12 files
    - can't open 8 gigs in memory so have to split up the files
    - also don't have to change the model archetechture

--- inst/jython/Met2ARL.py
+++ inst/jython/Met2ARL.py
@@ -8,33 +8,30 @@ from org.meteoinfo.projection import ProjectionInfo, Reproject
 from org.meteoinfo.global import Extent
 from org.meteoinfo.geoprocess.analysis import ResampleMethods
 from calendar import *
+import datetime as dt
 import os.path
 import sys
 import os
 import re
 
-def filterList(filter,list):
+def filterList(flter,lst):
 	result = []
-	for l in list:
-		match = re.search(filter,l)
+	for l in lst:
+		match = re.search(flter,l)
 		if match:
 			result += [l]
 	return result
 
 dirIn = sys.argv[1]
-pathOut = sys.argv[2]
+dirOut = sys.argv[2]
 
-#pathOut = "C:/Users/Siddarta.Jairam/Documents/ARL packed files/2066/NARRCAP/narrcap2066"
-#dirIn = "C:/Users/Siddarta.Jairam/Documents/NARCCAP/2066"
 
 #take file name and splice into a dictionary of:
 #{fullFile: var, ncvariable,level}
 
 varDict = {'pr':'TPP3','tas':'T02M','ps':'PRSS','uas':'U10M','vas':'V10M','husnp':'RELH0m',\
 	'hus':'RELH','ua':'UWND','va':'VWND', 'wa':'WWND','zg':'HGTS','ta':'TEMP'}
-#groundVars = ['PRSS','T02M','U10M','V10M']
-#atmVars = ['HGTS','TEMP','UWND','VWND','WWND','RELH']
-
+	
 ncDict = {"x":"hello"}
 groundVars = []
 atmVars = []
@@ -66,53 +63,80 @@ print groundVars
 
 #start and end index
 #for now just posit that only the first year is wanted
-startInd = 55
-endInd = 2975
+
 
 Met = MeteoDataInfo()
 
 #---- Set output ARL data info
-ARLDI = ARLDataInfo()
 #RH = 0.263*p(pa)*spH*1/[exp((17.67*(T-273.15))/(T-29.65))]
-
-for lv in levels:
-	ARLDI.levels.add(lv)
-	if lv == 0:
-		ARLDI.LevelVarList.add(groundVars)
-	else:
-		ARLDI.LevelVarList.add(atmVars)
-
 # Write ARL data file
 
-ARLDI.createDataFile(pathOut)
 
+exmPath = list(ncDict.keys())[0]
+Met.openNetCDFData(exmPath)
+NCDI = Met.getDataInfo()
+xs = NCDI.getXDimension().getValues()
+ys = NCDI.getYDimension().getValues()
+ts = NCDI.getTimes()
+mns = [x.getMonth() + 1 for x in ts.iterator()]
+inYrs = [x.getYear() + 1900 for x in ts.iterator()]
 
-modelName = 'NARRCAP'
-
-for path, ids in ncDict.iteritems():
-	print ids['ncVar']
-	Met.openNetCDFData(path)
-	NCDI = Met.getDataInfo()
-	ARLDI.X = NCDI.getXDimension().getValues()
-	ARLDI.Y = NCDI.getYDimension().getValues()
-	for ti in range(startInd,endInd):
-		Met.setTimeIndex(ti)
-		atime = NCDI.getTimes().get(ti)
-		dataHead = ARLDI.getDataHead(Met.getProjectionInfo(), 'FNL1', 2)
-		ARLDI.writeIndexRecord(atime, dataHead)
-		Met.setLevelIndex(ids['level'])
-		ncData = Met.getGridData(ids['ncVar'])
-		if ids['arlVar'] == 'PRSS' or ids['arlVar'] == 'WWND':
-			ncData = ncData.div(100)
-		if ids['arlVar'] == 'RELH':
-			ncData = ncData
-			#ncData = 0.263*p(pa)*ncData.mult(1/(exp((17.67*(T-273.15))/(T-29.65))))
-	label = DataLabel(atime)
-	label.setLevel(ids['level'])
-	label.setVarName(ids['arlVar'])
-	label.setGrid(99)
-	label.setForecast(0)
+for mn in range(1, 12):
+	print mn
+	ind = 0
+	yrWant = 2066
+	while not all([inYrs[ind] == yrWant, mns[ind] == mn]):
+		ind += 1
+	startInd = ind + 1
+	
+	while all([inYrs[ind] == yrWant, mns[ind] == mn]):
+		ind += 1
+	endInd = ind
+	
+# 	inThisYear = list([x == 2066 for x in inYrs])
+# 	inThisMonth = list([x == mn for x in mns])
+# 	
+# 	print list([inThisYear and inThisMonth])
+# 	startInd = map(f, inThisYear, inThisMonth])).index("True")
+# 	if mn == 12:
+# 		endInd = list([[x == 2067 for x in inYrs] and [x == mn for x in mns]]).index("True") - 1
+# 	else:
+# 		endInd = list([inThisYear and [x == mn + 1 for x in mns]]).index("True") - 1
+# 		
+	ARLDI = ARLDataInfo()
+	for lv in levels:
+		ARLDI.levels.add(lv)
+		if lv == 0:
+			ARLDI.LevelVarList.add(groundVars)
+		else:
+			ARLDI.LevelVarList.add(atmVars)
+	ARLDI.createDataFile(dirOut + "/" + month_abbr[mns[startInd +2]].lower() + "2066" + ".arl")
+	
+	
+	print startInd
+	print endInd
+	for path, ids in ncDict.iteritems():
+		
+		Met.openNetCDFData(path)
+		NCDI = Met.getDataInfo()
+		ARLDI.X = xs
+		ARLDI.Y = ys
+		for ti in range(startInd,endInd):
+			Met.setTimeIndex(ti)
+			atime = ts.get(ti)
+			dataHead = ARLDI.getDataHead(Met.getProjectionInfo(), 'FNL1', 2)
+			ARLDI.writeIndexRecord(atime, dataHead)
+			Met.setLevelIndex(ids['level'])
+			ncData = Met.getGridData(ids['ncVar'])
+			if ids['arlVar'] == 'PRSS' or ids['arlVar'] == 'WWND':
+				ncData = ncData.div(100)
+			if ids['arlVar'] == 'RELH':
+				ncData = ncData
+				#ncData = 0.263*p(pa)*ncData.mult(1/(exp((17.67*(T-273.15))/(T-29.65))))
+		label = DataLabel(atime)
+		label.setLevel(ids['level'])
+		label.setVarName(ids['arlVar'])
+		label.setGrid(99)
+		label.setForecast(0)
 	ARLDI.writeGridData(label, ncData)
-
-
-ARLDI.closeDataFile()
+	ARLDI.closeDataFile()
